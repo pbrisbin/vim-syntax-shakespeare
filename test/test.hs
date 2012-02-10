2b86f34ba33abe921490f611c6528b1aa78ac61e
patrick brisbin <pbrisbin@gmail.com>
2012-02-10 10:08:40 -0500

    make more efficient test cases

--- test/test.hs
+++ test/test.hs
@@ -1,10 +1,69 @@
-module Test where
+[hamlet|
+    <head>
+        <title>#{title}
+        <script src="/jquery.min.js">
+        <script src="/jquery-ui.min.js">
 
-qsort :: (Ord a) => [a] -> [a]
-qsort []     = []
-qsort (x:xs) = (qsort $ filter (< x) xs) ++ [x] ++ (qsort $ filter (>= x) xs)
+    \<!-- why you no work -->
 
-addCassius [$cassius|
+    <body>
+        <p>Go to 
+        <a href="@{RoutR}">
+
+        <p .something #{what}
+
+        <div id="accordion">
+            $forall tagGroup <- tagGroups
+                ^{addTagGroup tagGroup}
+
+
+        <script type="text/javascript">
+            $(function() {
+                $("#accordion").accordion({
+                    collapsible: true,
+                    autoHeight:  false,
+                    active:      false
+                });
+            });
+
+        <p>_{FooterMsg}
+    |]
+
+[hamlet|<a href=@{RouteR} title="foo">#{what}?|]
+
+[whamlet|
+    <head>
+        <h1>Foo
+
+    ^{stuff}
+|]
+
+[ihamlet|
+    <head>
+        <h1>Foo
+
+    _{i18nstuffs $ msg}
+|]
+
+[shamlet|
+    <p>
+        Some #{interpolations}
+    |]
+
+[xhamlet|
+    <xml>
+        <node>a #{a}
+        <node>b #{b}
+        <node>c @{CrouteR}
+    |]
+
+[xshamlet|
+    <xml>
+        <node>a #{a}
+        <node>b #{b}
+    |]
+
+[cassius|
     body
         /* comment */
         color: #ffffff
@@ -17,7 +76,23 @@ addCassius [$cassius|
         background-color: #ffffff
     |]
 
-addJulius [$julius|
+[lucius|
+    body {
+        /* comment */
+        color: #ffffff;
+        font-size: 100%;
+    }
+
+    .class {
+        background-color:
+    }
+
+    .#{classAsVariable} {
+        background-color: #ffffff;
+    }
+    |]
+
+[julius|
     // comment
     function f(_arg) {
         var g = _arg + #{f . g $ h};
@@ -25,42 +100,4 @@ addJulius [$julius|
     }
 
     f(1.0);
-    |]
-
--- test oneliners
-addHamlet [hamlet|<a href=@{RouteR} title="foo">#{what}?|]
-
-[whamlet|
-    <head>
-        <h1>Foo
-
-    _{i18nstuffs $ msg}
-|]
-
-addHamlet [$hamlet|
-    <header>
-        <h1>Foo
-
-    \<!-- is this valid hamlet? -->
-
-    <p .something #{what}
-    <article .fullpage>
-        <div id="accordion">
-            $forall tagGroup <- tagGroups
-                ^{addTagGroup tagGroup}
-
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js">
-    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js">
-
-    <script type="text/javascript">
-        $(function() {
-            $("#accordion").accordion({
-                collapsible: true,
-                autoHeight:  false,
-                active:      false
-            });
-        });
-
-    <p>
-        Footer info
     |]
